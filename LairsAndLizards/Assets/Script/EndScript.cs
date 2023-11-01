d8fadcb872ee657cf48262115eea542c63d2bd7a
SeanBarnyard <shanebarnard15@hotmail.com>
2023-10-31 22:08:45 -0400

    Finished end

--- LairsAndLizards/Assets/Script/EndScript.cs
+++ LairsAndLizards/Assets/Script/EndScript.cs
@@ -5,5 +5,23 @@ using TMPro;
 
 public class EndScript : MonoBehaviour
 {
-    TextMeshProUGUI wavesTxt;
+    public TextMeshProUGUI wavesTxt;
+
+
+    public void SceneGoto(string scene)
+    {
+        Globals.instance.GoToScene(scene);
+        Destroy(Globals.instance);
+    }
+
+    public void ExitGame()
+    {
+        Globals.instance.QUITGAME();
+    }
+
+
+    private void Start()
+    {
+        wavesTxt.text = "You made it to wave: " + Globals.instance.wave.ToString();
+    }
 }
